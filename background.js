62c141b7ccd6bafbe633ca54bc55bf1a2243013c
尾崎周也 <Shuya@ShuyaOsaki-MacBook-Pro.local>
2016-01-31 23:50:59 +0900

    あげるの間違えたから消す

--- background.js
+++
@@ -1,9 +0,0 @@
-// backgroundじゃDOM操作できないからscript.jsからもらう
-chrome.browserAction.setBadgeText({"text":"0"}); // default
-// script.jsから値をうけとる
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse){
-      chrome.browserAction.setBadgeText({"text":String(request.latency)});
-      sendResponse({message: "kanryou"});
-    }
-  );
